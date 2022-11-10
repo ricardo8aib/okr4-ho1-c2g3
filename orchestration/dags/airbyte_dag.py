b75e1709e78bdd546dd03285bc0f2eeb2010f00e
Ricardo Ibarra <raib1997@gmail.com>
2022-11-10 14:28:16 -0500

    Add airbyte dag

---
+++ orchestration/dags/airbyte_dag.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+
+with DAG(
+        dag_id="ExtractAndLoad",
+        schedule_interval="@daily",
+        start_date=datetime(2022, 9, 9),
+        dagrun_timeout=timedelta(minutes=30),
+        catchup=False
+) as dag:
+
+    rds_to_s3_task = AirbyteTriggerSyncOperator(
+        task_id="rds_to_s3_task",
+        airbyte_conn_id="airbyte_conn",
+        connection_id="ec890ba2-7a47-4a38-8f1e-4bd11aafaf94",
+        asynchronous=False,
+    )
+
+    rds_to_s3_task
